1d7f5258b8360a2bd518a960f6a6f11bbec680f8
abhi10359 <as1040@srmist.edu.in>
2026-02-23 16:34:54 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,22 @@
+public class UC4 {
+    public static void main(String[] args) {
+
+        // Store all banner lines using String array
+        String[] banner = {
+        " *****    *****    ******    ****** ",
+        "*     *  *     *   *     *   *      ",
+        "*     *  *     *   *     *   *      ",
+        "*     *  *     *   ******    ****** ",
+        "*     *  *     *   *               *",
+        "*     *  *     *   *               *",
+        " *****    *****    *         ****** "    
+        };
+
+        // Print banner using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+    
+
